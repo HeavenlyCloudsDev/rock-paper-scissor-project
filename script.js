e612b9f3a8877b949cef10469ff5196bfd4892b5
HeavenlyCloudsDev <173420105+HeavenlyCloudsDev@users.noreply.github.com>
2025-12-19 15:39:27 -0500

    planning/pseudocode but must be broken into smaller tasks

---
+++ script.js
@@ -0,0 +1,8 @@
+WRITE the logic to get the computer choice
+WRITE the logic to get the human choice
+DECLARE the players score variables
+WRITE the logic to play a single round
+WRITE the logic to play the entire game
+
+
+// make sure to break these down even smaller
