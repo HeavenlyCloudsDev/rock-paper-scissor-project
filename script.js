6047c1cbaec3397b884bca6e6bcab8d2869f01a9
HeavenlyCloudsDev <173420105+HeavenlyCloudsDev@users.noreply.github.com>
2025-12-20 21:35:21 -0500

    fixes and removal of if/else statements that were not needed, also added the scores for both the human and computer but are not initialized

--- script.js
+++ script.js
@@ -1,17 +1,49 @@
-// What is the problem? 
 
-// Create a game that allows computer to give a result randomly
-// This result is given when the player picks their choice.Create
-
-// WRITE the logic for the computers choice
-//     CREATE choices for the computer
-//     MAKE the choices random each time 
-//     SHOW the computers choice at the same time the user chooses theirs
-
-const computerChoices = ["rock", "paper", "scissors"];
-const randomIndex = Math.floor(Math.random() * computerChoices.length);
-const computerGuess = computerChoices[randomIndex];
+// CREATE a new function for the computers choice
+// WRITE a return code inside of the function declaration to return either string values: rock, paper, or scissors
+//     Look into how to get a Math.random method to work for a string
+// TEST to see if it will return the strings that were intended
 
 
-console.log(computerGuess);
+
+function getComputerChoice() {
+    let randomIndex = Math.floor(Math.random() * 3);
+
+    if (randomIndex === 0) {
+        return "Rock";
+    } else if (randomIndex === 1) {
+        return "Paper";
+    } else (randomIndex === 2) 
+        return "Scissors";
+    }
+
+
+    console.log(getComputerChoice());
+
+
+//CREATE a new function for the human choice
+//WRITE a code where the human/ user can enter their choice and return what they have put into the prompt
+//TEST to see if it will return the input using the function name
+//TEST 1 returned an undefined so I need to define choices?
+//TEST 2 gave an ReferenceError saying that humanChoice is not defined
+//TEST 3 the getHumanChoice function is still not working properly, it always returns Rock
+//TEST 4 successful!
+
+function getHumanChoice() {
+    prompt("Rock,Paper, or Scissors?");
+}
+    console.log(prompt(""));
+
+
+// DECLARE the player score variables
+// CREATE two new variables for the human score and the computer score in global scope
+//INIT both of the variables to have the value of 0 
+
+
+
+const humanScore = "Your Score:" + 0;
+const computerScore = "Their Score:" + 0;
+
+console.log(humanScore);
+console.log(computerScore);
 
